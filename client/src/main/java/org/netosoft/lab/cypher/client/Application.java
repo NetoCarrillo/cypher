64573c0ad10c8a1e04b141f0d7347bed08b1ab7e
Ernesto Carrillo <ernesto_brouwer@hotmail.com>
2016-08-12 11:57:48 -0500

    Documentation

--- client/src/main/java/org/netosoft/lab/cypher/client/Application.java
+++ client/src/main/java/org/netosoft/lab/cypher/client/Application.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.InvalidKeyException;
 import java.util.Map;
--- client/src/main/java/org/netosoft/lab/cypher/client/ClientApp.java
+++ client/src/main/java/org/netosoft/lab/cypher/client/ClientApp.java
@@ -7,19 +7,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.xml.bind.DatatypeConverter;
-import sun.security.rsa.RSAPrivateCrtKeyImpl;
 import sun.security.rsa.RSAPublicKeyImpl;
 
 /**
@@ -27,16 +20,25 @@ import sun.security.rsa.RSAPublicKeyImpl;
  * @author ernesto
  */
 public class ClientApp{
-	/** String to hold name of the encryption algorithm. */
+	/** Name of the encryption algorithm. */
 	private final String algorithm;
-	
+	/** Key to encrypt messages. */
 	private final PublicKey publicKey;
-//			"/home/ernesto/NetBeansProjects/lab-edu/output/cypher/public_1470867697609.key";
-//			"/home/ernesto/tmp/rsa/public_1.key";
-			
+	/** Host where the messages will be send. */
 	private final String host;
+	/** Port on the host to connect. */
 	private final int port;
 
+	/**
+	 *
+	 * @param algorithm
+	 * @param publicKeyPath
+	 * @param host
+	 * @param port
+	 * @throws IOException Error on the load of the file containing the public key.
+	 * @throws InvalidKeyException In case an erorr ocurres during the load of
+	 * the public key
+	 */
 	public ClientApp(String algorithm, String publicKeyPath, String host, int port) throws IOException, InvalidKeyException{
 		this.algorithm = algorithm;
 		this.host = host;
@@ -54,39 +56,47 @@ public class ClientApp{
 		}
 	}
 
+	/**
+	 * Sends a encrypted message to the host.
+	 * @param message the message.
+	 * @throws IOException Conextion error.
+	 * @throws ClassNotFoundException the type of the reciving message is not found.
+	 * @throws InvalidKeyException the key is not valid.
+	 */
 	public void sendMessage(String message) throws IOException, ClassNotFoundException, InvalidKeyException{
 		//establish socket connection to server
 		try(Socket socket = new Socket(host, port);
-				ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-				ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());){
+			ObjectOutputStream oos =
+					new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream ois =
+					new ObjectInputStream(socket.getInputStream());){
 
 			oos.writeObject(encryptMessage(message));
 				
 			String response = (String)ois.readObject();
 			
-			System.out.printf("Message sent: %s%nMessage recived: %s%nEquals: %b%n%n", message, response, message.equals(response));
+			//Shows that the sent encrypted message is the same decrypted by the server.
+			System.out.printf("Message sent: %s%nMessage recived: %s%nEquals: %b%n%n",
+					message, response, message.equals(response));
 		}
 	}
 	
-	private String encryptMessage(String words) throws IOException, InvalidKeyException{
-		// Encrypt the string using the public key
-		final byte[] cipherText = encrypt(words, publicKey);
-
-		return DatatypeConverter.printBase64Binary(cipherText);
-	}
-	
-	private byte[] encrypt(String text, PublicKey key){
-		byte[] cipherText = null;
+	private String encryptMessage(String text) throws IOException, InvalidKeyException{
+		String encrypted;
 		try{
 			// get an RSA cipher object and print the provider
 			final Cipher cipher = Cipher.getInstance(algorithm);
 			// encrypt the plain text using the public key
-			cipher.init(Cipher.ENCRYPT_MODE, key);
-			cipherText = cipher.doFinal(text.getBytes());
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+			
+			byte[] cipherText = cipher.doFinal(text.getBytes());
+			encrypted = DatatypeConverter.printBase64Binary(cipherText);
+			
 		}catch(GeneralSecurityException e){
+			encrypted = null;
 			e.printStackTrace(System.err);
 		}
-		return cipherText;
-	}
 		
+		return encrypted;
+	}
 }
